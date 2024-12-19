92e98221a2e874f9b6217a583a71107a6e98ca98
Ayoub Bousslama <a_bousslama@hetic.eu>
2024-12-19 01:39:14 +0100

    modification de noms de variables et implémentation du structure de code dèjà donné

--- compute_number_neighbors.py
+++ compute_number_neighbors.py
@@ -1,5 +1,29 @@
+import numpy
+
+frame = numpy.array([[0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 1, 1, 1, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0, 0, 0]])
+
+
 def compute_number_neighbors(paded_frame, index_line, index_column):
     
-    voisins = paded_frame[index_line-1:index_line+2, index_column-1:index_column+2]
-    number_neighbors = voisins.sum() - paded_frame[index_line, index_column]
+    the_neighbors = paded_frame[index_line-1:index_line+2, index_column-1:index_column+2]
+    number_neighbors = the_neighbors.sum() - paded_frame[index_line, index_column] #enlever la valeur de la cellule elle meme pour laisser que les voisins
     return number_neighbors
+
+
+
+def compute_next_frame(frame):
+    paded_frame = numpy.pad(frame, 1, mode="constant") #zéro padding
+
+
+    return frame
+
+while True:
+    
+    print(frame)
+    frame = compute_next_frame(frame)
