19dcbbadeb4578712557fa55d665ce40c0ba4233
Ayoub Bousslama <a_bousslama@hetic.eu>
2024-12-19 02:16:13 +0100

    correction des cas if

--- compute_number_neighbors.py
+++ compute_number_neighbors.py
@@ -1,4 +1,5 @@
 import numpy
+import time
 
 frame = numpy.array([[0, 0, 0, 0, 0, 0, 0],
                     [0, 0, 0, 0, 0, 0, 0],
@@ -19,16 +20,17 @@ def compute_number_neighbors(paded_frame, index_line, index_column):
 
 def compute_next_frame(frame):
     paded_frame = numpy.pad(frame, 1, mode="constant") #zéro padding
-    
+    modifications = [] #liste pour stocker les modifications et pouvoir travailler directement dans frame
     for index_line in range(1, len(paded_frame)-1):
         for index_column in range(1, len(paded_frame[index_line])-1):
             the_neighbors = compute_number_neighbors(paded_frame, index_line, index_column)
 
-            if the_neighbors < 2 or the_neighbors > 3:
-                    frame[index_line-1, index_column-1] = 0
-            else:
-                if the_neighbors == 3:
-                    frame[index_line-1, index_column-1] = 1
+            if paded_frame[index_line, index_column] == 1:  # Si la cellule est vivante
+                if the_neighbors < 2 or the_neighbors > 3:  # Deviens morte
+                    modifications.append((index_line-1, index_column-1, 0))
+            else:  # Cellule déjà morte
+                if the_neighbors == 3:  # Deviens vivante
+                    modifications.append((index_line-1, index_column-1, 1))
     return frame 
 
 while True:
